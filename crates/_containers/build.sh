92968a1d672f669a9dde37be2888b9827e6f2abf
Paul Butler <paul@driftingin.space>
2022-02-11 01:34:44 +0000

    skaffold build respects cache

---
+++ crates/_containers/build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Based on:
+# https://github.com/GoogleContainerTools/skaffold/blob/main/examples/custom-buildx/buildx.sh
+
+if ! docker buildx inspect skaffold-builder >/dev/null 2>&1; then
+  docker buildx create --name skaffold-builder --platform amd64
+fi
+
+BASE=$(echo $IMAGE | cut -d":" -f 1)-base
+
+set -x
+
+/usr/bin/docker buildx build \
+    --builder skaffold-builder \
+    --cache-from type=registry,ref=${BASE}:latest \
+    --cache-to type=registry,ref=${BASE}-base:latest,mode=max \
+    --file $1 \
+    --tag $IMAGE \
+    --load .
