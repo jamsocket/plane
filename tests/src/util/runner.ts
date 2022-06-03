de2df6f45cd8a35eeed338fbc4615f3da0bfa847
Paul Butler <paul@driftingin.space>
2022-06-03 15:33:12 +0000

    Fix prettier settings and add back tests

--- tests/src/util/runner.ts
+++ tests/src/util/runner.ts
@@ -1,53 +1,53 @@
-import { ChildProcess, spawn } from "child_process";
-import { DropHandler } from "./environment.js";
-import { KeyCertPair } from "./certificates.js";
-import { sleep } from "./sleep.js";
-import { PebbleResult } from "./docker.js";
-import getPort from "@ava/get-port";
+import { ChildProcess, spawn } from "child_process"
+import { DropHandler } from "./environment.js"
+import { KeyCertPair } from "./certificates.js"
+import { sleep } from "./sleep.js"
+import { PebbleResult } from "./docker.js"
+import getPort from "@ava/get-port"
 
-const MANIFEST_PATH = process.env.MANIFEST_PATH || "../Cargo.toml";
-const SPAWNER_PATH = "../target/debug/spawner";
-const CLUSTER_DOMAIN = "mydomain.test";
+const MANIFEST_PATH = process.env.MANIFEST_PATH || "../Cargo.toml"
+const SPAWNER_PATH = "../target/debug/spawner"
+const CLUSTER_DOMAIN = "mydomain.test"
 
 export function waitForExit(proc: ChildProcess): Promise<void> {
   return new Promise((accept, reject) => {
     proc.on("exit", () => {
       if (proc.exitCode === 0 || proc.signalCode) {
-        accept();
+        accept()
       } else {
-        reject(new Error(`Process exited with non-zero code ${proc.exitCode}`));
+        reject(new Error(`Process exited with non-zero code ${proc.exitCode}`))
       }
-    });
-  });
+    })
+  })
 }
 
 export async function killProcAndWait(proc: ChildProcess): Promise<void> {
-  proc.kill("SIGTERM");
-  await waitForExit(proc);
+  proc.kill("SIGTERM")
+  await waitForExit(proc)
 }
 
 export interface ServeResult {
-  httpPort: number;
-  httpsPort?: number;
+  httpPort: number
+  httpsPort?: number
 }
 
 export class DroneRunner implements DropHandler {
-  server?: ChildProcess;
+  server?: ChildProcess
 
   constructor(private dbPath: string) {}
 
   async drop() {
     if (this.server !== undefined) {
-      await killProcAndWait(this.server);
+      await killProcAndWait(this.server)
     }
   }
 
   static build(): Promise<void> {
     const proc = spawn("cargo", ["build", "--manifest-path", MANIFEST_PATH], {
       stdio: "inherit",
-    });
+    })
 
-    return waitForExit(proc);
+    return waitForExit(proc)
   }
 
   async migrate() {
@@ -57,9 +57,9 @@ export class DroneRunner implements DropHandler {
       {
         stdio: "inherit",
       }
-    );
+    )
 
-    await waitForExit(proc);
+    await waitForExit(proc)
   }
 
   async certRefresh(
@@ -88,14 +88,14 @@ export class DroneRunner implements DropHandler {
           SPAWNER_TEST_ALLOWED_CERTIFICATE: pebble.cert,
         },
       }
-    );
+    )
 
-    await waitForExit(proc);
+    await waitForExit(proc)
   }
 
   async serve(certs?: KeyCertPair): Promise<ServeResult> {
-    const httpPort = await getPort();
-    let httpsPort;
+    const httpPort = await getPort()
+    let httpsPort
 
     const args = [
       "--cluster-domain",
@@ -104,10 +104,10 @@ export class DroneRunner implements DropHandler {
       this.dbPath,
       "--http-port",
       httpPort.toString(),
-    ];
+    ]
 
     if (certs !== undefined) {
-      httpsPort = await getPort();
+      httpsPort = await getPort()
 
       args.push(
         "--https-port",
@@ -116,25 +116,25 @@ export class DroneRunner implements DropHandler {
         certs.privateKeyPath,
         "--https-certificate",
         certs.certificatePath
-      );
+      )
     }
 
-    args.push("serve", "--proxy");
+    args.push("serve", "--proxy")
 
     const proc = spawn(SPAWNER_PATH, args, {
       stdio: "inherit",
-    });
+    })
 
     proc.on("exit", (code) => {
       if (code !== null) {
         // Server process should not exit until we kill it.
-        throw new Error(`Process exited with code ${code}.`);
+        throw new Error(`Process exited with code ${code}.`)
       }
-    });
+    })
 
-    this.server = proc;
-    await sleep(500);
+    this.server = proc
+    await sleep(500)
 
-    return { httpPort, httpsPort };
+    return { httpPort, httpsPort }
   }
 }
--- tests/src/util/websocket.ts
+++ tests/src/util/websocket.ts
@@ -1,72 +1,72 @@
-import { WebSocket } from "ws";
+import { WebSocket } from "ws"
 
-type AcceptRejectPair = [(st: string) => void, (er: Error) => void];
+type AcceptRejectPair = [(st: string) => void, (er: Error) => void]
 
 export class WebSocketClient {
-  ws: WebSocket;
-  incomingQueue: Array<string> = [];
-  promiseQueue: Array<AcceptRejectPair> = [];
-  closed = false;
+  ws: WebSocket
+  incomingQueue: Array<string> = []
+  promiseQueue: Array<AcceptRejectPair> = []
+  closed = false
 
   private constructor(url: string, host: string) {
-    this.ws = new WebSocket(url, { headers: { host } });
+    this.ws = new WebSocket(url, { headers: { host } })
   }
 
   static async create(url: string, host: string): Promise<WebSocketClient> {
-    const client = new WebSocketClient(url, host);
+    const client = new WebSocketClient(url, host)
 
     client.ws.on("message", (msg) => {
-      const decodedMsg = msg.toString("utf8");
+      const decodedMsg = msg.toString("utf8")
       if (client.promiseQueue.length > 0) {
-        const [accept] = client.promiseQueue.shift() as AcceptRejectPair;
-        accept(decodedMsg);
+        const [accept] = client.promiseQueue.shift() as AcceptRejectPair
+        accept(decodedMsg)
       } else {
-        client.incomingQueue.push(decodedMsg);
+        client.incomingQueue.push(decodedMsg)
       }
-    });
+    })
 
     await new Promise((accept, reject) => {
-      client.ws.on("open", accept);
+      client.ws.on("open", accept)
 
       client.ws.on("close", () => {
-        client.closed = true;
+        client.closed = true
         for (const acceptRejectPair of client.promiseQueue) {
-          const [, reject] = acceptRejectPair;
-          reject(new Error("WebSocket connection closed."));
+          const [, reject] = acceptRejectPair
+          reject(new Error("WebSocket connection closed."))
         }
 
-        reject(new Error("WebSocket connection closed before opening."));
-      });
+        reject(new Error("WebSocket connection closed before opening."))
+      })
 
       client.ws.on("error", (err) => {
-        reject(err);
-      });
-    });
+        reject(err)
+      })
+    })
 
-    return client;
+    return client
   }
 
   close(): void {
-    this.ws.close();
+    this.ws.close()
   }
 
   send(data: string): void {
-    this.ws.send(data);
+    this.ws.send(data)
   }
 
   async receive(): Promise<string> {
     if (this.closed) {
       return new Promise((_, reject) =>
         reject(new Error("WebSocket connection is already closed."))
-      );
+      )
     }
 
     if (this.incomingQueue.length > 0) {
-      return this.incomingQueue.shift();
+      return this.incomingQueue.shift()
     }
 
     return new Promise((accept, reject) => {
-      this.promiseQueue.push([accept, reject]);
-    });
+      this.promiseQueue.push([accept, reject])
+    })
   }
 }
