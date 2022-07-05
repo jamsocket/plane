9dfeb7a01079aa6b27d7d4236657297dc488f6a8
Paul Butler <paul@driftingin.space>
2022-07-05 15:56:54 +0000

    Cleaner API for duration units

--- tests/src/test-agent.ts
+++ tests/src/test-agent.ts
@@ -27,12 +27,6 @@ test("Drone sends ready messages", async (t) => {
   const natsPort = await t.context.docker.runNats()
   await sleep(100)
   const nats = await connect({ port: natsPort, token: "mytoken" })
-
-  const connectionRequestSubscription =
-    new NatsMessageIterator<DroneConnectRequest>(
-      nats.subscribe("drone.register")
-    )
-
   t.context.runner.runAgent(natsPort)
 
   // Initial handshake.
@@ -86,7 +80,7 @@ test("NATS logs", async (t) => {
   const request: SpawnRequest = {
     image: TEST_IMAGE,
     backend_id: backendId,
-    max_idle_time: { secs: 10, nanos: 0 },
+    max_idle_secs: 10,
     env: {
       PORT: "8080",
     },
@@ -123,7 +117,7 @@ test("Spawn with agent", async (t) => {
   const request: SpawnRequest = {
     image: TEST_IMAGE,
     backend_id: backendId,
-    max_idle_time: { secs: 10, nanos: 0 },
+    max_idle_secs: 10,
     env: {
       PORT: "8080",
     },
@@ -184,7 +178,7 @@ test("Spawn fails during start", async (t) => {
   const request: SpawnRequest = {
     image: TEST_IMAGE,
     backend_id: backendId,
-    max_idle_time: { secs: 10, nanos: 0 },
+    max_idle_secs: 10,
     env: {
       PORT: "8080",
       EXIT_CODE: "1",
@@ -236,7 +230,7 @@ test("Backend fails after ready", async (t) => {
   const request: SpawnRequest = {
     image: TEST_IMAGE,
     backend_id: backendId,
-    max_idle_time: { secs: 10, nanos: 0 },
+    max_idle_secs: 10,
     env: {
       PORT: "8080",
     },
