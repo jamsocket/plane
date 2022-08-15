f2f5ae767899a48249792e2c329ee141631ae940
Abhishek Cherath <abhi@driftingin.space>
2022-08-15 15:59:56 -0400

    remove comment

--- src/drone/cert/mod.rs
+++ src/drone/cert/mod.rs
@@ -47,10 +47,6 @@ pub async fn get_certificate(
                 let value_b64 = eab_key_b64;
                 let value = base64::decode_config(&value_b64, base64::URL_SAFE)
                     .expect("cannot decode base64 value");
-                /*
-                let value = openssl::base64::decode_block(&value_b64)
-                    .expect("openssl cannot decode base64 value");
-                */
                 PKey::hmac(&value).unwrap()
             };
             builder.external_account_binding(kid.to_string(), eab_key);
