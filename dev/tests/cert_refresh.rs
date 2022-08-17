5efd48e5cbc8568caeb6b7daa728c02f50bd56f9
Paul Butler <paul@driftingin.space>
2022-08-17 15:40:33 +0000

    refactor cert test a bit

--- dev/tests/cert_refresh.rs
+++ dev/tests/cert_refresh.rs
@@ -3,8 +3,41 @@ use dev::{
     resources::{nats::nats, pebble::pebble},
     timeout::{spawn_timeout, timeout},
 };
-use dis_spawner::messages::cert::SetAcmeDnsRecord;
+use dis_spawner::{messages::cert::SetAcmeDnsRecord, nats::TypedNats};
 use integration_test::integration_test;
+use openssl::x509::X509;
+use tokio::{time::error::Elapsed, task::JoinHandle};
+
+fn collect_alt_names(cert: &X509) -> Vec<String> {
+    cert
+        .subject_alt_names()
+        .iter()
+        .map(|d| format!("{:?}", d))
+        .collect()
+}
+
+struct DummyDnsHandler {
+    handle: JoinHandle<Result<Result<(), anyhow::Error>, Elapsed>>
+}
+
+impl DummyDnsHandler {
+    pub async fn new(conn: &TypedNats, expect_domain: &str) -> Result<DummyDnsHandler> {
+        let expect_domain = expect_domain.to_owned();
+        let mut dns_sub = conn.subscribe(SetAcmeDnsRecord::subject()).await?;
+        let handle = spawn_timeout(10_000, async move {
+            let message = dns_sub.next().await.unwrap().unwrap();
+            assert_eq!(expect_domain, message.value.cluster);
+            message.respond(&true).await?;
+            Ok(())
+        });
+
+        Ok(DummyDnsHandler { handle })
+    }
+
+    pub async fn finish(self) -> Result<()> {
+        self.handle.await??
+    }
+}
 
 #[integration_test]
 async fn test_cert_refresh() -> Result<()> {
@@ -12,13 +45,7 @@ async fn test_cert_refresh() -> Result<()> {
     let pebble = pebble().await?;
     let conn = nats.connection().await?;
 
-    let mut dns_sub = conn.subscribe(SetAcmeDnsRecord::subject()).await?;
-    let handle = spawn_timeout(10_000, async move {
-        let message = dns_sub.next().await.unwrap().unwrap();
-        assert_eq!("spawner.test", message.value.cluster);
-        message.respond(&true).await?;
-        Ok(())
-    });
+    let dns_handler = DummyDnsHandler::new(&conn, "spawner.test").await?;
 
     let (_, cert) = timeout(
         60_000,
@@ -34,14 +61,9 @@ async fn test_cert_refresh() -> Result<()> {
     )
     .await?;
 
-    handle.await???;
+    dns_handler.finish().await?;
 
-    let alt_names: Vec<String> = cert
-        .subject_alt_names()
-        .iter()
-        .map(|d| format!("{:?}", d))
-        .collect();
-
+    let alt_names = collect_alt_names(&cert);
     assert_eq!(vec!["[*.spawner.test]".to_string()], alt_names);
 
     Ok(())
