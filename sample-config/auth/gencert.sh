f08d374c9f39ad4843f922e645e009ccf4aec227
Paul Butler <paul@driftingin.space>
2022-10-02 10:43:08 -0400

    get certificates to work

--- sample-config/auth/gencert.sh
+++ sample-config/auth/gencert.sh
@@ -1,13 +1,44 @@
 #!/bin/sh
 
+# Create CA key + cert
 openssl req \
     -nodes \
     -x509 \
-    -newkey \
-    rsa:4096 \
-    -keyout key.pem \
-    -out cert.pem \
+    -newkey rsa:4096 \
+    -keyout ca-key.pem \
+    -out ca-cert.pem \
     -sha256 \
     -days 3650 \
-    -subj "/C=US/ST=NY/L=New York/O=Spawner/OU=Org/CN=spawner.test" \
-    -addext "subjectAltName = DNS:*.spawner.test"
+    -subj "/C=US/ST=NY/L=New York/O=Spawner/OU=Org/CN=spawner.test"
+
+# Create a certificate signing request.
+openssl req \
+    -nodes \
+    -new \
+    -newkey rsa:4096 \
+    -keyout site-key.pem \
+    -out site-cert.csr \
+    -subj "/C=US/ST=NY/L=New York/O=Spawner/OU=Org/CN=spawner.test"
+
+echo "subjectAltName = DNS:*.spawner.test" > extfile
+
+# Sign the certificate.
+openssl x509 \
+    -req \
+    -in site-cert.csr \
+    -days 365 \
+    -CA ca-cert.pem \
+    -CAkey ca-key.pem \
+    -CAcreateserial \
+    -out site-cert.pem \
+    -extfile extfile
+
+# cat ca-cert.pem site-cert.pem > site-cert-tmp.pem
+# mv site-cert-tmp.pem site-cert.pem
+
+# Clean up
+rm \
+    extfile \
+    ca-cert.srl \
+    ca-key.pem \
+    site-cert.csr
