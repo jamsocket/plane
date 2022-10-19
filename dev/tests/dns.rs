df46c03d2b508ab766c77c9a15b69233d16ebc9d
Paul Butler <paul@driftingin.space>
2022-10-19 10:19:32 -0400

    add tests for A records

--- dev/tests/dns.rs
+++ dev/tests/dns.rs
@@ -1,5 +1,4 @@
-use std::{net::SocketAddr, time::Duration, str::Utf8Error};
-use anyhow::{anyhow, Result};
+use std::{net::{SocketAddr, Ipv4Addr}, time::Duration, str::Utf8Error};
 use integration_test::integration_test;
 use plane_controller::{config::DnsOptions, dns::serve_dns, plan::DnsPlan};
 use plane_core::{nats::TypedNats, Never, messages::dns::{SetDnsRecord, DnsRecordType}, types::ClusterName};
@@ -10,8 +9,9 @@ use plane_dev::{
 };
 use trust_dns_resolver::{
     config::{NameServerConfig, Protocol, ResolverConfig, ResolverOpts},
-    TokioAsyncResolver, error::ResolveErrorKind, lookup::TxtLookup, proto::rr::rdata::TXT
+    TokioAsyncResolver, error::{ResolveErrorKind, ResolveError}
 };
+use anyhow::Result;
 
 const DNS_PORT: u16 = 5353;
 
@@ -19,6 +19,24 @@ struct DnsServer {
     _guard: LivenessGuard<Result<Never, anyhow::Error>>,
     resolver: TokioAsyncResolver,
     pub nc: TypedNats,
+}
+
+trait DnsResultExt {
+    fn is_nxdomain(&self) -> bool;
+}
+
+impl<T> DnsResultExt for Result<T, ResolveError> {
+    fn is_nxdomain(&self) -> bool {
+        match self {
+            Err(e) => {
+                match e.kind() {
+                    ResolveErrorKind::NoRecordsFound { .. } => true,
+                    _ => false,
+                }
+            },
+            Ok(_) => false
+        }
+    }
 }
 
 impl DnsServer {
@@ -51,22 +69,6 @@ impl DnsServer {
         })
     }
 
-    async fn expect_no_a_record(&self, domain: &str) -> Result<()> {
-        let result = self.resolver.ipv4_lookup(domain).await;
-
-        match result {
-            Err(e) => {
-                match e.kind() {
-                    ResolveErrorKind::NoRecordsFound { .. } => Ok(()),
-                    _ => Err(anyhow!("Expected NXDOMAIN, got another error {:?}", e)),
-                }
-            },
-            Ok(result) => {
-                Err(anyhow!("Expected NXDOMAIN, got {:?}", result))
-            }
-        }
-    }
-
     async fn txt_record(&self, domain: &str) -> Result<Vec<String>> {
         let result = self.resolver.txt_lookup(domain).await?;
 
@@ -76,13 +78,19 @@ impl DnsServer {
 
         Ok(c?)
     }
+
+    async fn a_record(&self, domain: &str) -> std::result::Result<Vec<Ipv4Addr>, ResolveError> {
+        let result = self.resolver.ipv4_lookup(domain).await?;
+
+        Ok(result.into_iter().collect())
+    }
 }
 
 #[integration_test]
 async fn dns_bad_request() -> Result<()> {
     let dns = DnsServer::new().await?;
 
-    dns.expect_no_a_record("foo.bar").await?;
+    assert!(dns.a_record("foo.bar").await.is_nxdomain());
 
     Ok(())
 }
@@ -102,6 +110,79 @@ async fn dns_txt_record() -> Result<()> {
 
     let result = dns.txt_record("_acme-challenge.plane.test").await?;
     assert_eq!(vec!["foobar".to_string()], result);
+
+    Ok(())
+}
+
+#[integration_test]
+async fn dns_multi_txt_record() -> Result<()> {
+    let dns = DnsServer::new().await?;
+
+    dns.nc.publish_jetstream(&SetDnsRecord {
+        cluster: ClusterName::new("plane.test"),
+        kind: DnsRecordType::TXT,
+        name: "_acme-challenge".into(),
+        value: "foobar".into(),
+    }).await?;
+
+    dns.nc.publish_jetstream(&SetDnsRecord {
+        cluster: ClusterName::new("plane.test"),
+        kind: DnsRecordType::TXT,
+        name: "_acme-challenge".into(),
+        value: "foobaz".into(),
+    }).await?;
+
+    tokio::time::sleep(Duration::from_secs(1)).await;
+
+    let result = dns.txt_record("_acme-challenge.plane.test").await?;
+    assert_eq!(vec!["foobar".to_string(), "foobaz".to_string()], result);
+
+    Ok(())
+}
+
+#[integration_test]
+async fn dns_a_record() -> Result<()> {
+    let dns = DnsServer::new().await?;
+
+    dns.nc.publish_jetstream(&SetDnsRecord {
+        cluster: ClusterName::new("plane.test"),
+        kind: DnsRecordType::A,
+        name: "louie".into(),
+        value: "12.12.12.12".into(),
+    }).await?;
+
+    tokio::time::sleep(Duration::from_secs(1)).await;
+
+    let result = dns.a_record("louie.plane.test").await?;
+    assert_eq!(vec![Ipv4Addr::new(12, 12, 12, 12)], result);
+
+    Ok(())
+}
+
+#[integration_test]
+async fn dns_multi_a_record() -> Result<()> {
+    let dns = DnsServer::new().await?;
+
+    dns.nc.publish_jetstream(&SetDnsRecord {
+        cluster: ClusterName::new("plane.test"),
+        kind: DnsRecordType::A,
+        name: "louie".into(),
+        value: "12.12.12.12".into(),
+    }).await?;
+
+    tokio::time::sleep(Duration::from_secs(1)).await;
+
+    dns.nc.publish_jetstream(&SetDnsRecord {
+        cluster: ClusterName::new("plane.test"),
+        kind: DnsRecordType::A,
+        name: "louie".into(),
+        value: "14.14.14.14".into(),
+    }).await?;
+
+    tokio::time::sleep(Duration::from_secs(1)).await;
+
+    let result = dns.a_record("louie.plane.test").await?;
+    assert_eq!(vec![Ipv4Addr::new(14, 14, 14, 14)], result);
 
     Ok(())
 }
