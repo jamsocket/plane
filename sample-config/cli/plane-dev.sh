064372a56ab0ab040ef5cda3ac89de32a1bdb091
Abhishek Cherath <abhi@driftingin.space>
2022-10-19 18:11:15 -0400

    long opts

--- sample-config/cli/plane-dev.sh
+++ sample-config/cli/plane-dev.sh
@@ -6,6 +6,16 @@ usage() {
 	exit 1
 }
 
+long_opts() {
+	case $1 in
+	linux) [ ! "$OS" ] && linux=1 OS=1 || exit 1;;
+	macos) [ ! "$OS" ] && macos=1 OS=1 || exit 1;;
+	x11) [ ! "$BROWSER_OPT" ] && x11=1 || exit 1;;
+	guac) [ ! "$BROWSER_OPT" ] && guac=1 || exit 1;;	
+	?) usage ""
+	esac
+}
+
 if [ ! -d "$PWD/compose" ]
 then
 	echo "This script must be run from the plane/sample-config directory"
@@ -17,13 +27,14 @@ DOCKER_COMPOSE="docker compose"
 
 
 linux='' macos='' x11='' guac='' OS='' BROWSER_OPT=''
-while getopts "lmxg" arg
+while getopts "lmxg-:" arg
 do
 	case $arg in 
 	l) [ ! "$OS" ] && linux=1 OS=1 || exit 1;;
 	m) [ ! "$OS" ] && macos=1 OS=1 || exit 1;;
 	x) [ ! "$BROWSER_OPT" ] && x11=1 || exit 1;;
 	g) [ ! "$BROWSER_OPT" ] && guac=1 || exit 1;;
+	-) long_opts "$OPTARG" || exit 1;;
 	?) usage ""
 	esac
 done
@@ -36,7 +47,7 @@ fi
 
 if { [ $linux ] || [ $macos ]; } && [ $guac ]
 then
-	$DOCKER_COMPOSE -f "$COMPOSE_FILE_DIR/plane.yml" -f "$COMPOSE_FILE_DIR/firefox-guac.yml" up --remove-orphans --force-recreate --build & xdg-open "http://localhost:3000"
+	$DOCKER_COMPOSE -f "$COMPOSE_FILE_DIR/plane.yml" up --remove-orphans --force-recreate --build & xdg-open "http://localhost:3000"
 	exit 0
 fi
 
@@ -45,3 +56,5 @@ then
 	echo "unimplemented!"
 	exit 1
 fi
+
+usage ""
