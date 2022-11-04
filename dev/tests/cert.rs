6004ec8c39853296ab165afd8267dd2c118ea994
Paul Butler <paul@driftingin.space>
2022-11-04 10:46:59 -0400

    Send both SetDnsRecord and SetAcmeDnsRecord for interim period

--- dev/tests/cert.rs
+++ dev/tests/cert.rs
@@ -1,7 +1,12 @@
 use anyhow::Result;
 use integration_test::integration_test;
 use openssl::x509::X509;
-use plane_core::{messages::cert::SetAcmeDnsRecord, nats::TypedNats, types::ClusterName};
+use plane_core::{
+    messages::cert::SetAcmeDnsRecord,
+    messages::dns::{DnsRecordType, SetDnsRecord},
+    nats::TypedNats,
+    types::ClusterName,
+};
 use plane_dev::{
     resources::{nats::Nats, pebble::Pebble},
     scratch_dir,
@@ -18,27 +23,46 @@ fn collect_alt_names(cert: &X509) -> Vec<String> {
 }
 
 struct DummyDnsHandler {
-    handle: JoinHandle<Result<()>>,
+    new_handle: JoinHandle<Result<()>>,
+    old_handle: JoinHandle<Result<()>>,
 }
 
 impl DummyDnsHandler {
     pub async fn new(conn: &TypedNats, expect_domain: &str) -> Result<DummyDnsHandler> {
-        let expect_domain = ClusterName::new(expect_domain);
-        let mut dns_sub = conn
-            .subscribe(SetAcmeDnsRecord::subscribe_subject())
-            .await?;
-        let handle = spawn_timeout(10_000, "Should get ACME DNS request.", async move {
-            let message = dns_sub.next().await.unwrap();
-            assert_eq!(expect_domain, message.value.cluster);
-            message.respond(&true).await?;
-            Ok(())
-        });
+        let old_handle = {
+            // Old DNS message
+            let expect_domain = ClusterName::new(expect_domain);
+            let mut dns_sub = conn
+                .subscribe(SetAcmeDnsRecord::subscribe_subject())
+                .await?;
+            spawn_timeout(10_000, "Should get ACME DNS request.", async move {
+                let message = dns_sub.next().await.unwrap();
+                assert_eq!(expect_domain, message.value.cluster);
+                message.respond(&true).await?;
+                Ok(())
+            })
+        };
 
-        Ok(DummyDnsHandler { handle })
+        let new_handle = {
+            // New DNS message
+            let expect_domain = ClusterName::new(expect_domain);
+            let mut dns_sub = conn.subscribe(SetDnsRecord::subscribe_subject()).await?;
+            spawn_timeout(10_000, "Should get ACME DNS request.", async move {
+                let message = dns_sub.next().await.unwrap();
+                assert_eq!(expect_domain, message.value.cluster);
+                assert_eq!("_acme-challenge", &message.value.name);
+                assert_eq!(DnsRecordType::TXT, message.value.kind);
+                Ok(())
+            })
+        };
+
+        Ok(DummyDnsHandler { new_handle, old_handle })
     }
 
     pub async fn finish(self) -> Result<()> {
-        self.handle.await?
+        self.new_handle.await??;
+        self.old_handle.await??;
+        Ok(())
     }
 }
 
