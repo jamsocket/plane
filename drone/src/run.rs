63670d2867b2675c88420adcbed9c5099841bdfd
Paul Butler <paul@driftingin.space>
2022-11-04 13:03:34 -0400

    fix clippy issues

--- drone/src/run.rs
+++ drone/src/run.rs
@@ -79,7 +79,7 @@ async fn drone_main() -> NeverResult {
 }
 
 pub fn run() -> Result<()> {
-    let mut signals = Signals::new(&[SIGINT, SIGTERM])?;
+    let mut signals = Signals::new([SIGINT, SIGTERM])?;
 
     thread::spawn(move || {
         for _ in signals.forever() {
