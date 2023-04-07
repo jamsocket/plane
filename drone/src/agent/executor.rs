80d2a6cedd9223c3e46efa95fbf2889269335d66
Paul Butler <paul@driftingin.space>
2023-04-07 13:31:24 -0400

    Update tokio version and fix tests

--- drone/src/agent/executor.rs
+++ drone/src/agent/executor.rs
@@ -253,7 +253,7 @@ impl<E: Engine> Executor<E> {
                 Ok(Some(new_state)) => {
                     state = new_state;
 
-                    if state.running() {
+                    if state.running() && !self.backend_to_monitor.contains_key(&spawn_request.backend_id) {
                         self.backend_to_monitor.insert(
                             spawn_request.backend_id.clone(),
                             BackendMonitor::new(
