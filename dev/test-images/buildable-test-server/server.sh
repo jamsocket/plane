1c149a5fd030c9f13d7984bd88e0aaa437d0954b
Abhishek Cherath <abhi@quic.us>
2023-04-20 16:42:01 -0400

    new build image inprog

---
+++ dev/test-images/buildable-test-server/server.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+while true; do
+	cat <<-EOF  | busybox nc -l -p 8080
+	HTTP/1.1 200 OK
+	Content-Type: text/html
+	Connection: close
+
+	<html><body><h1>Hello, World!</h1></body></html>'
+	EOF
+done 
