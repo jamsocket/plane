3ad948d60fbff6fe5d2d5bcc7853cf0fa6553370
Abhishek Cherath <abhi@quic.us>
2023-04-25 14:11:26 -0400

    buildable server!
    a little fun note here, the shell can't interrupt you, even to handle signals.
    so a signal handler requires a command to actually finish before running handlers
    on enqueued signals. which is why here I've set the busybox nc timeout to 1s

--- dev/test-images/buildable-test-server/server.sh
+++ dev/test-images/buildable-test-server/server.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
-# shellcheck enable=all
-
-set -x
-
 export EXIT_TIMEOUT
 export EXIT_CODE
 
-trap 'exit ${EXIT_CODE}' TERM
-[ -n "${EXIT_TIMEOUT}" ] && { sleep "${EXIT_TIMEOUT}" && kill -TERM "${PID}" & }
+trap 'exit ${EXIT_CODE}' USR1 
+[ -n "${EXIT_TIMEOUT}" ] && { sleep "${EXIT_TIMEOUT}" && kill -USR1 "$$" ; } &
 
 while true; do
-	cat <<-EOF  | busybox nc -l -p 8080
+	cat <<-EOF  | busybox nc -w 1 -l -p 8080 2>/dev/null
 	HTTP/1.1 200 OK
 	Content-Type: text/html
 	Connection: close
