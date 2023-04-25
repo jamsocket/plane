1c14ceb2a3c2e23a5b8354790eeb2c35fa7c6886
Abhishek Cherath <abhi@quic.us>
2023-04-25 14:13:30 -0400

    alternatively, and more sensibly, use a subshell

--- dev/test-images/buildable-test-server/server.sh
+++ dev/test-images/buildable-test-server/server.sh
@@ -5,12 +5,13 @@ export EXIT_CODE
 trap 'exit ${EXIT_CODE}' USR1 
 [ -n "${EXIT_TIMEOUT}" ] && { sleep "${EXIT_TIMEOUT}" && kill -USR1 "$$" ; } &
 
-while true; do
-	cat <<-EOF  | busybox nc -w 1 -l -p 8080 2>/dev/null
+( while true; do
+	cat <<-EOF  | busybox nc -l -p 8080
 	HTTP/1.1 200 OK
 	Content-Type: text/html
 	Connection: close
 
 	<html><body><h1>Hello, World!</h1></body></html>'
 	EOF
-done 
+done ) &
+wait
