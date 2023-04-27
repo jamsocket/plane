1b050e97aba8ae66083bcdc397db3eeed313c317
Abhishek Cherath <abhi@quic.us>
2023-04-27 15:29:14 -0400

    tests pass

--- dev/test-images/buildable-test-server/resources/server.sh
+++ dev/test-images/buildable-test-server/resources/server.sh
@@ -3,11 +3,9 @@ export EXIT_TIMEOUT
 export EXIT_CODE
 set -x
 
-hello() {
-	echo -e "HTTP/1.1 200 OK\r\nConnection: Close\r\nContent-Type: text/plain\r\n\r\nHello World!"
-}
-
-trap 'exit ${EXIT_CODE}' EXIT 
-[ -n "${EXIT_TIMEOUT}" ] && { sleep "${EXIT_TIMEOUT}" && kill -EXIT "$$" ; } &
-busybox httpd  -p 8080 -c ./httpd.conf -h . 
+[ -n "${EXIT_TIMEOUT}" ] && { sleep "${EXIT_TIMEOUT}" && kill -USR1 "$$" ; } &
+busybox httpd -vf -p 8080 -c ./httpd.conf -h . &
+trap "kill $!" USR1
 wait
+exit "${EXIT_CODE:-0}"
+
