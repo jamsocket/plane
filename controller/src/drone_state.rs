dda773751d0b4c35c63aa2e2854c42a6811daf50
Paul Butler <paul@driftingin.space>
2023-05-11 14:25:58 -0400

    Remove assignment from backend message (#372)
    * Remove assignment from backend message
    
    * add test of assignment state update

--- controller/src/drone_state.rs
+++ controller/src/drone_state.rs
@@ -69,15 +69,6 @@ fn convert_to_state_message(
                     },
                 }),
             },
-            WorldStateMessage {
-                cluster: msg.cluster.clone(),
-                message: ClusterStateMessage::BackendMessage(BackendMessage {
-                    backend: msg.backend.clone(),
-                    message: BackendMessageType::Assignment {
-                        drone: msg.drone.clone(),
-                    },
-                }),
-            },
         ],
     }
 }
@@ -275,20 +266,10 @@ mod test {
                         timestamp: time,
                     },
                 }),
-            },
-            WorldStateMessage {
-                cluster: ClusterName::new("plane.test"),
-                message: ClusterStateMessage::BackendMessage(BackendMessage {
-                    backend: backend.clone(),
-                    message: BackendMessageType::Assignment {
-                        drone: drone.clone(),
-                    },
-                }),
-            },
+            }
         ];
 
         assert_eq!(state_message, expected);
         assert!(!state_message.first().unwrap().overwrite());
-        assert!(state_message.last().unwrap().overwrite());
     }
 }
