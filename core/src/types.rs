bc21983f5038c614cd5823bf64ce19a5a2db1106
Abhishek Cherath <abhi@quic.us>
2023-05-23 17:00:38 -0400

    correct property for subject component

--- core/src/types.rs
+++ core/src/types.rs
@@ -46,7 +46,7 @@ pub trait AsSubjectComponent {
 
 impl AsSubjectComponent for BackendId {
     fn as_subject_component(&self) -> String {
-        self.to_resource_name()
+		self.id().into()
     }
 }
 
