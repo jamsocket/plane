3a24990ed6f6f6dd185d13135e2c533d7c5377b4
Paul Butler <paul@driftingin.space>
2023-09-29 06:57:57 -0400

    Use path-based routing for the local docker compose example. (#454)
    * Doc updates
    
    * Remove Firefox/dnsmasq-based docker compose and document path-based routing

--- sample-config/firefox-common-config/firstrun.js
+++
@@ -1,5 +0,0 @@
-// Firefox shows some messages the first time it is run, but because we're running in
-// an ephemeral environment, these start to get annoying. This file disables them.
-
-pref("datareporting.policy.firstRunURL", "");
-pref("datareporting.policy.dataSubmissionPolicyBypassNotification", true);
