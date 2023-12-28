bba6cba16a690b5700befeb663519e80d8e06ff5
Paul Butler <paul@driftingin.space>
2023-12-28 18:54:53 -0500

    add beginning of getting started guide

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -8,16 +7,8 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.png" />
-        <meta property="og:image" content="https://y-sweet.cloud/og-image.png" />
-        <meta name="twitter:image" content="https://y-sweet.cloud/og-image.png" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@drifting_corp" />
-        <meta name="twitter:title" content="Y-Sweet Cloud" />
-        <meta name="twitter:description" content="a Yjs service with persistence and auth" />
-        <title>Y-Sweet Cloud: the WebSocket sync backend you’ll love</title>
       </Head>
       <Component {...pageProps} />
-      <Script defer data-domain="y-sweet.cloud" src="/js/script.js"></Script>
     </>
   )
 }
--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -1,8 +1,17 @@
+import { useRouter } from 'next/router'
 import Logo from './components/logo'
 
 export default {
-    logo: <div style={{height: 25}}><Logo /></div>,
+    logo: <div style={{ height: 25 }}><Logo /></div>,
     project: {
         link: 'https://github.com/drifting-in-space/plane'
+    },
+    useNextSeoProps() {
+        const { asPath } = useRouter()
+        if (asPath !== '/') {
+            return {
+                titleTemplate: '%s – Plane'
+            }
+        }
     }
 }
