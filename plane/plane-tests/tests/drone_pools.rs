c76959c1cf2e4a2fe7d5be3f59804dc826fd8369
Michael Silver <me@michaelsilver.io>
2024-02-09 16:47:14 -0500

    Add tests

---
+++ plane/plane-tests/tests/drone_pools.rs
@@ -0,0 +1,114 @@
+use crate::common::timeout::WithTimeout;
+use common::test_env::TestEnvironment;
+use plane::{
+    client::PlaneClient,
+    names::BackendName,
+    types::{
+        BackendStatus, ConnectRequest, ExecutorConfig, KeyConfig, PullPolicy, ResourceLimits,
+        SpawnConfig,
+    },
+};
+use plane_test_macro::plane_test;
+use serde_json::Map;
+use std::collections::HashMap;
+
+mod common;
+
+async fn wait_until_terminated(client: &PlaneClient, backend_id: &BackendName) {
+    let mut backend_status_stream = client
+        .backend_status_stream(backend_id)
+        .with_timeout(10)
+        .await
+        .unwrap()
+        .unwrap();
+
+    while let Ok(Some(message)) = backend_status_stream.next().with_timeout(10).await {
+        tracing::info!("Got status: {:?}", message);
+        if message.status == BackendStatus::Terminated {
+            break;
+        }
+    }
+}
+
+#[plane_test]
+async fn drone_pools(env: TestEnvironment) {
+    let controller = env.controller().await;
+    let client = controller.client();
+    let pool = "test".to_string();
+    let _drone = env.drone(&controller).await;
+    let _drone_in_pool = env.drone_in_pool(&controller, &pool).await;
+
+    // Wait for the drone to register. TODO: this seems long.
+    tokio::time::sleep(std::time::Duration::from_secs(5)).await;
+
+    tracing::info!("Requesting backend.");
+    let connect_request = ConnectRequest {
+        spawn_config: Some(SpawnConfig {
+            id: None,
+            cluster: Some(env.cluster.clone()),
+            executable: ExecutorConfig {
+                image: "ghcr.io/drifting-in-space/demo-image-drop-four".to_string(),
+                pull_policy: Some(PullPolicy::IfNotPresent),
+                env: HashMap::default(),
+                resource_limits: ResourceLimits::default(),
+                credentials: None,
+            },
+            lifetime_limit_seconds: Some(5),
+            max_idle_seconds: None,
+            use_static_token: false,
+        }),
+        key: Some(KeyConfig {
+            name: "reuse-key".to_string(),
+            namespace: "".to_string(),
+            tag: "".to_string(),
+        }),
+        user: None,
+        auth: Map::default(),
+        pool: None,
+    };
+
+    let response = client.connect(&connect_request).await.unwrap();
+    tracing::info!("Got response.");
+
+    assert!(response.spawned);
+
+    tracing::info!("Requesting backend from pool.");
+    let mut connect_request_with_pool = connect_request.clone();
+    connect_request_with_pool.pool = Some(pool);
+    connect_request_with_pool.key = Some(KeyConfig {
+        name: "pool-specific-key".to_string(),
+        ..connect_request.key.unwrap()
+    });
+
+    let response_from_pool = client.connect(&connect_request_with_pool).await.unwrap();
+    tracing::info!("Got response from pool.");
+
+    assert!(response_from_pool.spawned);
+    let response_from_pool_drone = response_from_pool.drone.clone().unwrap();
+    assert_ne!(response_from_pool_drone, response.drone.unwrap());
+
+    tracing::info!("Requesting backend from pool with different key.");
+    let mut connect_request_with_pool_different_key = connect_request_with_pool.clone();
+    connect_request_with_pool_different_key.key = Some(KeyConfig {
+        name: "different-key".to_string(),
+        ..connect_request_with_pool.key.unwrap()
+    });
+
+    let response_from_pool_different_key = client
+        .connect(&connect_request_with_pool_different_key)
+        .await
+        .unwrap();
+    tracing::info!("Got response from pool with different key.");
+
+    assert!(response_from_pool_different_key.spawned);
+    assert_eq!(
+        response_from_pool_different_key.drone.unwrap(),
+        response_from_pool_drone
+    );
+
+    tracing::info!("Waiting for all backends to terminate.");
+    wait_until_terminated(&client, &response.backend_id).await;
+    wait_until_terminated(&client, &response_from_pool.backend_id).await;
+    wait_until_terminated(&client, &response_from_pool_different_key.backend_id).await;
+    tracing::info!("All backends terminated.");
+}
