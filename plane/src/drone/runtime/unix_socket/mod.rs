898cf2d0bffa288be5c49bfc671062977cc2685a
Michael Silver <michael@jamsocket.com>
2024-06-05 17:56:04 -0400

    Better send/receive logic

--- plane/src/drone/runtime/unix_socket/mod.rs
+++ plane/src/drone/runtime/unix_socket/mod.rs
@@ -10,6 +10,7 @@ use crate::{
 };
 use anyhow::anyhow;
 use anyhow::{Error, Result};
+use dashmap::DashMap;
 use futures_util::Stream;
 use serde::{Deserialize, Serialize};
 use serde_json;
@@ -17,7 +18,6 @@ use std::net::SocketAddr;
 use std::sync::Arc;
 use tokio::{io::{AsyncReadExt, AsyncWriteExt}, sync::{mpsc, oneshot}};
 use tokio::net::UnixStream;
-use tokio::sync::Mutex;
 use tokio_util::codec::{Framed, LengthDelimitedCodec};
 use tokio_serde::formats::Json;
 use tokio_serde::Framed as SerdeFramed;
@@ -102,51 +102,45 @@ pub struct UnixSocketRuntimeConfig {
 pub struct UnixSocketRuntime {
     config: UnixSocketRuntimeConfig,
     tx: mpsc::Sender<RuntimeCommand>,
-    rx: Arc<Mutex<mpsc::Receiver<RuntimeResponse>>>,
+    response_map: Arc<DashMap<Uuid, oneshot::Sender<RuntimeResponse>>>,
 }
 
 impl UnixSocketRuntime {
     pub async fn new(socket_path: String) -> Result<Self> {
         let stream = UnixStream::connect(&socket_path).await?;
         let (tx, rx) = mpsc::unbounded_channel();
-        let (response_tx, response_rx) = mpsc::unbounded_channel();
+        let response_map = Arc::new(DashMap::new());
 
-        tokio::spawn(handle_connection(stream, rx, response_tx));
+        tokio::spawn(handle_connection(stream, rx, Arc::clone(&response_map)));
 
         Ok(UnixSocketRuntime {
             config: UnixSocketRuntimeConfig { socket_path },
             tx,
-            rx: Arc::new(Mutex::new(response_rx)),
+            response_map,
         })
     }
 
     async fn send_command(&self, command: RuntimeCommand) -> Result<RuntimeResponse, Error> {
         let (response_tx, response_rx) = oneshot::channel();
+        let id = match &command {
+            RuntimeCommand::Prepare { id, .. }
+            | RuntimeCommand::Spawn { id, .. }
+            | RuntimeCommand::Terminate { id, .. }
+            | RuntimeCommand::MetricsCallback { id }
+            | RuntimeCommand::WaitForBackend { id, .. } => *id,
+        };
+
+        self.response_map.insert(id, response_tx);
         self.tx.send(command).await?;
         let response = response_rx.await?;
         Ok(response)
-    }
-
-    async fn wait_for_response(&self, id: Uuid) -> Result<RuntimeResponse, Error> {
-        let mut rx = self.rx.lock().await;
-        while let Some(response) = rx.recv().await {
-            match response {
-                RuntimeResponse::Ok { id: resp_id }
-                | RuntimeResponse::SpawnResponse { id: resp_id, .. }
-                | RuntimeResponse::Error { id: resp_id, .. } if resp_id == id => {
-                    return Ok(response);
-                }
-                _ => continue,
-            }
-        }
-        Err(anyhow!("Failed to receive response"))
     }
 }
 
 async fn handle_connection(
     stream: UnixStream,
     mut rx: mpsc::Receiver<RuntimeCommand>,
-    response_tx: mpsc::Sender<RuntimeResponse>,
+    response_map: Arc<DashMap<Uuid, oneshot::Sender<RuntimeResponse>>>,
 ) -> Result<(), Box<dyn Error>> {
     let length_delimited = Framed::new(stream, LengthDelimitedCodec::new());
     let mut framed = SerdeFramed::new(length_delimited, Json::<RuntimeCommand, RuntimeResponse>::default());
@@ -154,9 +148,16 @@ async fn handle_connection(
     // Task to handle receiving messages
     let recv_task = tokio::spawn(async move {
         while let Some(Ok(msg)) = framed.try_next().await {
-            if response_tx.send(msg).await.is_err() {
-                eprintln!("failed to send response");
-                return;
+            let id = match &msg {
+                RuntimeResponse::Ok { id }
+                | RuntimeResponse::SpawnResponse { id, .. }
+                | RuntimeResponse::Error { id, .. } => *id,
+            };
+
+            if let Some(tx) = response_map.remove(&id).map(|(_, tx)| tx) {
+                let _ = tx.send(msg);
+            } else {
+                eprintln!("No sender found for response ID: {:?}", id);
             }
         }
     });
@@ -181,11 +182,11 @@ impl Runtime for UnixSocketRuntime {
 
     async fn prepare(&self, config: &Self::BackendConfig) -> Result<(), Error> {
         let id = Uuid::new_v4();
-        self.tx.send(RuntimeCommand::Prepare {
+        let command = RuntimeCommand::Prepare {
             id,
             config: config.clone(),
-        }).await?;
-        self.wait_for_response(id).await?.into()
+        };
+        self.send_command(command).await?.into()
     }
 
     async fn spawn(
@@ -196,24 +197,24 @@ impl Runtime for UnixSocketRuntime {
         static_token: Option<&BearerToken>,
     ) -> Result<SpawnResult, Error> {
         let id = Uuid::new_v4();
-        self.tx.send(RuntimeCommand::Spawn {
+        let command = RuntimeCommand::Spawn {
             id,
             backend_id: backend_id.to_string(),
             executable,
             acquired_key: acquired_key.map(|k| k.to_string()),
             static_token: static_token.map(|t| t.to_string()),
-        }).await?;
-        self.wait_for_response(id).await?.into()
+        };
+        self.send_command(command).await?.into()
     }
 
     async fn terminate(&self, backend_id: &BackendName, hard: bool) -> Result<(), Error> {
         let id = Uuid::new_v4();
-        self.tx.send(RuntimeCommand::Terminate {
+        let command = RuntimeCommand::Terminate {
             id,
             backend_id: backend_id.to_string(),
             hard,
-        }).await?;
-        self.wait_for_response(id).await?.into()
+        };
+        self.send_command(command).await?.into()
     }
 
     async fn metrics_callback<F: Fn(BackendMetricsMessage) + Send + Sync + 'static>(
@@ -239,11 +240,11 @@ impl Runtime for UnixSocketRuntime {
         address: SocketAddr,
     ) -> Result<(), BackendError> {
         let id = Uuid::new_v4();
-        self.tx.send(RuntimeCommand::WaitForBackend {
+        let command = RuntimeCommand::WaitForBackend {
             id,
             backend_id: backend.to_string(),
             address: address.to_string(),
-        }).await?;
-        self.wait_for_response(id).await?.into()
+        };
+        self.send_command(command).await?.into()
     }
 }
