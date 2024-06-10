616fe0eb21c71781abc567ce7ecc6b49ffb598f1
Michael Silver <michael@jamsocket.com>
2024-06-10 10:44:31 -0400

    Add server

--- plane/src/typed_unix_socket/mod.rs
+++ plane/src/typed_unix_socket/mod.rs
@@ -1,13 +1,11 @@
-use dashmap::DashMap;
 use serde::{Deserialize, Serialize};
-use std::sync::Arc;
-use tokio::sync::{broadcast, mpsc, oneshot};
 use uuid::Uuid;
 
 pub mod client;
+pub mod server;
 
-#[derive(Serialize, Deserialize)]
-struct IDedMessage<T> {
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct IDedMessage<T> {
     id: Uuid,
     message: T,
 }
@@ -23,54 +21,3 @@ enum WrappedClientMessageType<RequestType, ClientMessageType> {
     Request(IDedMessage<RequestType>),
     ClientMessage(ClientMessageType),
 }
-
-struct TypedUnixSocketClient<ClientMessageType, ServerMessageType, RequestType, ResponseType>
-where
-    ClientMessageType: Send + Sync + 'static,
-    ServerMessageType: Send + Sync + 'static + Clone,
-    RequestType: Send + Sync + 'static,
-    ResponseType: Send + Sync + 'static,
-{
-    // stream: UnixStream,
-    tx: mpsc::UnboundedSender<WrappedClientMessageType<RequestType, ClientMessageType>>,
-    // rx: mpsc::UnboundedReceiver<WrappedClientMessageType<RequestType, ClientMessageType>>,
-    response_map: Arc<DashMap<Uuid, oneshot::Sender<ResponseType>>>,
-    event_tx: broadcast::Sender<ServerMessageType>,
-}
-
-// struct TypedUnixSocket<ClientMessageType, ServerMessageType, RequestType, ResponseType>
-// where
-//     ClientMessageType: Send + Sync + 'static,
-//     ServerMessageType: Send + Sync + 'static + Clone,
-//     RequestType: Send + Sync + 'static,
-//     ResponseType: Send + Sync + 'static,
-// {
-//     socket_path: PathBuf,
-// }
-
-// impl<ClientMessageType, ServerMessageType, RequestType, ResponseType>
-//     TypedUnixSocket<ClientMessageType, ServerMessageType, RequestType, ResponseType>
-// where
-//     ClientMessageType: Send + Sync + 'static,
-//     ServerMessageType: Send + Sync + 'static + Clone,
-//     RequestType: Send + Sync + 'static,
-//     ResponseType: Send + Sync + 'static,
-// {
-//     pub fn new<P: AsRef<Path>>(socket_path: P) -> Self {
-//         Self {
-//             socket_path: socket_path.into(),
-//         }
-//     }
-
-//     pub async fn create_client(
-//         &self,
-//     ) -> TypedUnixSocketClient<ClientMessageType, ServerMessageType, RequestType, ResponseType>
-//     {
-//         TypedUnixSocketClient::new(&self.socket_path).await.unwrap()
-//     }
-
-//     // pub async fn create_server(&self) -> TypedUnixSocketServer<ClientMessageType, ServerMessageType> {
-//     //     //TODO
-//     //     unimplemented!()
-//     // }
-// }
