jamsocket/plane

commit bfb39b11e1194c21a30691abb9f5ef8f6b2e7c5b

Handle `BackendError::Other` in `wait_for_backend()` (#799)
https://github.com/jamsocket/plane/pull/766 introduced
`BackendError::Other` as a way to handle misc errors that may occur in
the `wait_for_backend()` executor function. However, the PR failed to
handle that error type in `BackendManager`. As a result, spawns would
succeed in spite of that error.

Introduce `TerminationReason::ErrorWaiting` and hard terminate any
backend that encounters `BackendError::Other` in the
`wait_for_backend()` step, logging the specifics. (Use a `match`
statement to ensure future additions to the error struct get caught in
this codepath during compilation.)

plane/src/drone/backend_manager.rs
@@ -197,13 +197,16 @@ impl BackendManager {
                 let backend_id = self.backend_id.clone();
                 let runtime = self.runtime.clone();
                 StepStatusResult::future_status(async move {
-                    if let Err(BackendError::StartupTimeout) =
-                        runtime.wait_for_backend(&backend_id, address.0).await
-                    {
-                        tracing::error!("Backend startup timeout");
-                        state.to_hard_terminating(TerminationReason::StartupTimeout)
-                    } else {
-                        state.to_ready(address)
+                    match runtime.wait_for_backend(&backend_id, address.0).await {
+                        Ok(()) => state.to_ready(address),
+                        Err(BackendError::StartupTimeout) => {
+                            tracing::error!("Backend startup timeout");
+                            state.to_hard_terminating(TerminationReason::StartupTimeout)
+                        }
+                        Err(BackendError::Other(msg)) => {
+                            tracing::error!("Failed to wait for backend: {}", msg);
+                            state.to_hard_terminating(TerminationReason::ErrorWaiting)
+                        }
                     }
                 })
             }

plane/src/types/backend_state.rs
@@ -236,6 +236,7 @@ pub enum TerminationReason {
     KeyExpired,
     Lost,
     StartupTimeout,
+    ErrorWaiting,
 }
 
 impl valuable::Valuable for TerminationReason {
@@ -246,6 +247,7 @@ impl valuable::Valuable for TerminationReason {
             TerminationReason::KeyExpired => valuable::Value::String("key_expired"),
             TerminationReason::Lost => valuable::Value::String("lost"),
             TerminationReason::StartupTimeout => valuable::Value::String("startup_timeout"),
+            TerminationReason::ErrorWaiting => valuable::Value::String("error_waiting"),
         }
     }
 

commit 03df75de94eaadc777fadb1903103d75d4736f9a

Rename TerminationReason::ErrorWaiting => InternalError (#802)
Renaming error status introduced in
https://github.com/jamsocket/plane/pull/799 so that it's more user
friendly.

plane/src/drone/backend_manager.rs
@@ -205,7 +205,7 @@ impl BackendManager {
                         }
                         Err(BackendError::Other(msg)) => {
                             tracing::error!("Failed to wait for backend: {}", msg);
-                            state.to_hard_terminating(TerminationReason::ErrorWaiting)
+                            state.to_hard_terminating(TerminationReason::InternalError)
                         }
                     }
                 })

plane/src/types/backend_state.rs
@@ -236,7 +236,7 @@ pub enum TerminationReason {
     KeyExpired,
     Lost,
     StartupTimeout,
-    ErrorWaiting,
+    InternalError,
 }
 
 impl valuable::Valuable for TerminationReason {
@@ -247,7 +247,7 @@ impl valuable::Valuable for TerminationReason {
             TerminationReason::KeyExpired => valuable::Value::String("key_expired"),
             TerminationReason::Lost => valuable::Value::String("lost"),
             TerminationReason::StartupTimeout => valuable::Value::String("startup_timeout"),
-            TerminationReason::ErrorWaiting => valuable::Value::String("error_waiting"),
+            TerminationReason::InternalError => valuable::Value::String("internal_error"),
         }
     }
 