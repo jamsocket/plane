77dada1c17cf1852af7df87530c6ffb59d84ee56
Paul Butler <paul@driftingin.space>
2024-09-24 06:43:38 -0400

    add bad gateway test

--- plane/plane-tests/tests/proxy.rs
+++ plane/plane-tests/tests/proxy.rs
@@ -1,5 +1,12 @@
+use std::{net::SocketAddr, str::FromStr};
+
 use common::{proxy_mock::MockProxy, test_env::TestEnvironment};
-use plane::{protocol::RouteInfoResponse, types::BearerToken};
+use plane::{
+    log_types::BackendAddr,
+    names::{BackendName, Name},
+    protocol::{RouteInfo, RouteInfoResponse},
+    types::{BearerToken, ClusterName, SecretToken},
+};
 use plane_test_macro::plane_test;
 use reqwest::StatusCode;
 
@@ -40,4 +47,36 @@ async fn proxy_bad_bearer_token(env: TestEnvironment) {
     let response = handle.await.unwrap();
 
     assert_eq!(response.status(), StatusCode::UNAUTHORIZED);
+}
+
+#[plane_test]
+async fn proxy_backend_unreachable(env: TestEnvironment) {
+    let mut proxy = MockProxy::new().await;
+    let url = format!("http://{}/abc123/", proxy.addr());
+    let handle = tokio::spawn(async { reqwest::get(url).await.expect("Failed to send request") });
+
+    let route_info_request = proxy.recv_route_info_request().await;
+    assert_eq!(
+        route_info_request.token,
+        BearerToken::from("abc123".to_string())
+    );
+
+    proxy
+        .send_route_info_response(RouteInfoResponse {
+            token: BearerToken::from("abc123".to_string()),
+            route_info: Some(RouteInfo {
+                backend_id: BackendName::new_random(),
+                address: BackendAddr(SocketAddr::from(([123, 234, 123, 234], 12345))),
+                secret_token: SecretToken::from("secret".to_string()),
+                cluster: ClusterName::from_str("test").unwrap(),
+                user: None,
+                user_data: None,
+                subdomain: None,
+            }),
+        })
+        .await;
+
+    let response = handle.await.unwrap();
+
+    assert_eq!(response.status(), StatusCode::BAD_GATEWAY);
 }
