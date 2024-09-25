404c031b0d86f39f1ddda495025a7d95f5741b34
Paul Butler <paul@driftingin.space>
2024-09-25 10:29:18 -0400

    add test for websocket connection monitor

---
+++ plane/plane-tests/tests/common/websocket_echo_server.rs
@@ -0,0 +1,61 @@
+use axum::{
+    extract::ws::{Message, WebSocket, WebSocketUpgrade},
+    response::IntoResponse,
+    routing::get,
+    Router,
+};
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+
+/// A websocket echo server that echos messages back to the client.
+pub struct WebSocketEchoServer {
+    handle: tokio::task::JoinHandle<()>,
+    addr: SocketAddr,
+}
+
+#[allow(unused)]
+impl WebSocketEchoServer {
+    pub async fn new() -> Self {
+        let app = Router::new().route("/ws", get(ws_handler));
+
+        let addr = SocketAddr::from(([127, 0, 0, 1], 0));
+        let tcp_listener = TcpListener::bind(addr).await.unwrap();
+        let addr = tcp_listener.local_addr().unwrap();
+
+        let handle = tokio::spawn(async move {
+            axum::serve(tcp_listener, app.into_make_service())
+                .await
+                .unwrap();
+        });
+
+        Self { handle, addr }
+    }
+
+    pub fn addr(&self) -> SocketAddr {
+        self.addr
+    }
+}
+
+impl Drop for WebSocketEchoServer {
+    fn drop(&mut self) {
+        self.handle.abort();
+    }
+}
+
+async fn ws_handler(ws: WebSocketUpgrade) -> impl IntoResponse {
+    ws.on_upgrade(handle_socket)
+}
+
+async fn handle_socket(mut socket: WebSocket) {
+    while let Some(msg) = socket.recv().await {
+        if let Ok(msg) = msg {
+            if let Message::Text(text) = msg {
+                if socket.send(Message::Text(text)).await.is_err() {
+                    break;
+                }
+            }
+        } else {
+            break;
+        }
+    }
+}
--- plane/plane-tests/tests/proxy_connection_monitor.rs
+++ plane/plane-tests/tests/proxy_connection_monitor.rs
@@ -1,6 +1,7 @@
 use common::{
     localhost_resolver::localhost_client, proxy_mock::MockProxy,
     simple_axum_server::SimpleAxumServer, test_env::TestEnvironment,
+    websocket_echo_server::WebSocketEchoServer,
 };
 use plane::{
     log_types::BackendAddr,
@@ -11,6 +12,7 @@ use plane::{
 use plane_test_macro::plane_test;
 use reqwest::StatusCode;
 use std::str::FromStr;
+use tokio_tungstenite::connect_async;
 
 mod common;
 
@@ -24,13 +26,11 @@ async fn proxy_marks_backend_as_recently_active(env: TestEnvironment) {
     let cluster = ClusterName::from_str(&format!("plane.test:{}", port)).unwrap();
     let url = format!("http://plane.test:{port}/abc123/");
     let client = localhost_client();
-    println!("sending request");
-    let handle = tokio::spawn(client.get(url).send());
 
     let backend_entry = proxy.backend_entry(&backend_name);
     assert!(backend_entry.is_none());
 
-    println!("waiting for route info request");
+    let handle = tokio::spawn(client.get(url).send());
     let route_info_request = proxy.recv_route_info_request().await;
     assert_eq!(
         route_info_request.token,
@@ -53,14 +53,62 @@ async fn proxy_marks_backend_as_recently_active(env: TestEnvironment) {
         })
         .await;
 
-    println!("waiting for response");
     let response = handle.await.unwrap().unwrap();
     assert_eq!(response.status(), StatusCode::OK);
-    println!("received response");
 
     let Some(backend_entry) = proxy.backend_entry(&backend_name) else {
         panic!("Backend entry not found");
     };
+    assert_eq!(backend_entry.active_connections, 0);
+    assert_eq!(backend_entry.had_recent_connection, true);
+}
+
+#[plane_test]
+async fn proxy_marks_websocket_backend_as_active(env: TestEnvironment) {
+    let server = WebSocketEchoServer::new().await;
+    let backend_name = BackendName::new_random();
+
+    let mut proxy = MockProxy::new().await;
+    let port = proxy.port();
+    let cluster = ClusterName::from_str(&format!("localhost:{}", port)).unwrap();
+    let url = format!("ws://localhost:{port}/abc123/ws");
+
+    let handle = tokio::spawn(connect_async(url));
+
+    let route_info_request = proxy.recv_route_info_request().await;
+    assert_eq!(
+        route_info_request.token,
+        BearerToken::from("abc123".to_string())
+    );
+
+    proxy
+        .send_route_info_response(RouteInfoResponse {
+            token: BearerToken::from("abc123".to_string()),
+            route_info: Some(RouteInfo {
+                backend_id: backend_name.clone(),
+                address: BackendAddr(server.addr()),
+                secret_token: SecretToken::from("secret".to_string()),
+                cluster,
+                user: None,
+                user_data: None,
+                subdomain: None,
+            }),
+        })
+        .await;
+
+    let (mut ws_stream, _) = handle.await.unwrap().unwrap();
+
+    let Some(backend_entry) = proxy.backend_entry(&backend_name) else {
+        panic!("Backend entry not found");
+    };
+    assert_eq!(backend_entry.active_connections, 1);
+    assert_eq!(backend_entry.had_recent_connection, true);
+
+    ws_stream.close(None).await.unwrap();
+    drop(ws_stream);
+
+    tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+    let backend_entry = proxy.backend_entry(&backend_name).unwrap();
     assert_eq!(backend_entry.active_connections, 0);
     assert_eq!(backend_entry.had_recent_connection, true);
 }
