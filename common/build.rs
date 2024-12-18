2acdd0368512b0107e91404c50e39dc032e26920
Paul Butler <paul@driftingin.space>
2024-12-18 16:25:40 -0400

    Allow use as library

--- common/build.rs
+++ common/build.rs
@@ -1,13 +1,14 @@
 use std::process::Command;
 
 fn main() {
-    let output = Command::new("git")
+    let git_hash = Command::new("git")
         .args(["rev-parse", "HEAD"])
         .output()
-        .expect("Failed to execute git command");
-
-    let git_hash = String::from_utf8_lossy(&output.stdout).trim().to_string();
-    let git_hash: String = git_hash.chars().take(8).collect();
+        .map(|output| {
+            let hash = String::from_utf8_lossy(&output.stdout).trim().to_string();
+            hash.chars().take(8).collect::<String>()
+        })
+        .unwrap_or_else(|_| String::from("unknown"));
 
     println!("cargo:rustc-env=GIT_HASH={}", git_hash);
 }
